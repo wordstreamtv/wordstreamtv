019b810150a455c216fa8de4b673b66d15c2ee86
wordstreamtv <wordstreamtv>
2022-01-31 08:47:58 -0600

    GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
+
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-KX9G7KR' });
+  }, []);
   return <Component {...pageProps} />
 }
 
